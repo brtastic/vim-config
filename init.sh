67844a7ad0c7e9b02bbc1a4d8ab4fa7544eb37e9
brtastic <brtastic.dev@gmail.com>
2019-11-19 12:09:22 +0100

    Symlinks

--- init.sh
+++ init.sh
@@ -9,6 +9,8 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 printf "/tags\n/.ctags\n*~\n/.gutctags\n.ignore" >> ~/.gitignore
 git config --global core.excludesfile ~/.gitignore
+ln -s ~/config/.vimrc ~/.vimrc
+ln -s ~/config/.tmux.conf ~/.tmux.conf
 
 # OS-dependent:
 #sudo apt-get install silversearcher-ag
