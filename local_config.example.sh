d31f2decc5ad2a06aba452c9307d799667fa37b9
brtastic <brtastic.dev@gmail.com>
2020-07-13 23:16:47 +0200

    Move variables around for a better freebsd config

--- local_config.example.sh
+++ local_config.example.sh
@@ -3,3 +3,6 @@
 # xhost + local:
 # export DISPLAY=localhost:0
 # export MON_CONFIG="HDMI1;eDP1;right"
+export WALLPAPER="$HOME/pix/wallpaper.jpg"
+export LC_CTYPE=pl_PL.UTF-8
+export LANG=pl_PL.UTF-8
