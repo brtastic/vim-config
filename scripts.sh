d7570a738a06135603bc6481bfc7506ef65521ca
brtastic <brtastic.dev@gmail.com>
2021-02-08 19:36:01 +0100

    Todo script

--- scripts.sh
+++ scripts.sh
@@ -6,4 +6,5 @@ ln -s ~/config/scripts/sched ~/bin/sched
 ln -s ~/config/scripts/mon ~/bin/mon
 ln -s ~/config/scripts/touchpadtgl ~/bin/touchpadtgl
 ln -s ~/config/scripts/vi-workspace ~/bin/vi-workspace
-ln -s ~/config/scripts/kbtgl ~/bin/kbtgl
+ln -s ~/config/scripts/kbgtl ~/bin/kbgtl
+ln -s ~/config/scripts/todo ~/bin/todo
