cc8da1b674f7f016e45dc16c8e2da1ad552fb8cb
brtastic <brtastic.dev@gmail.com>
2021-06-21 00:46:43 +0200

    More FreeBSD-specific config

--- scripts.sh
+++ scripts.sh
@@ -8,3 +8,5 @@ ln -s ~/config/scripts/touchpadtgl ~/bin/touchpadtgl
 ln -s ~/config/scripts/vi-workspace ~/bin/vi-workspace
 ln -s ~/config/scripts/kbgtl ~/bin/kbgtl
 ln -s ~/config/scripts/todo ~/bin/todo
+ln -s ~/config/scripts/todo ~/bin/jrun
+ln -s ~/config/scripts/todo ~/bin/jroot
