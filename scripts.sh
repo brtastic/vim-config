d0eb5155b623c611ee73710a3d26ebb4848100fe
brtastic <brtastic.dev@gmail.com>
2021-06-27 10:36:19 +0200

    Further configuration of Thinkpad

--- scripts.sh
+++ scripts.sh
@@ -4,10 +4,12 @@ mkdir ~/bin
 ln -s ~/config/scripts/pstatus ~/bin/pstatus
 ln -s ~/config/scripts/sched ~/bin/sched
 ln -s ~/config/scripts/mon ~/bin/mon
+ln -s ~/config/scripts/mnt ~/bin/mnt
 ln -s ~/config/scripts/touchpadtgl ~/bin/touchpadtgl
 ln -s ~/config/scripts/vi-workspace ~/bin/vi-workspace
 ln -s ~/config/scripts/kbgtl ~/bin/kbgtl
 ln -s ~/config/scripts/todo ~/bin/todo
 ln -s ~/config/scripts/jrun ~/bin/jrun
 ln -s ~/config/scripts/jroot ~/bin/jroot
+ln -s ~/config/scripts/webcamstart ~/bin/webcamstart
 doas cp ~/config/scripts/brightness /usr/local/bin/brightness
