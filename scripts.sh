553c6741f77387f36b59fa1d6931851a00268456
bbrtj <brtastic.dev@gmail.com>
2022-02-04 17:51:18 +0100

    scripts - fix typo

--- scripts.sh
+++ scripts.sh
@@ -7,7 +7,7 @@ ln -s ~/config/scripts/mon ~/bin/mon
 ln -s ~/config/scripts/mnt ~/bin/mnt
 ln -s ~/config/scripts/touchpadtgl ~/bin/touchpadtgl
 ln -s ~/config/scripts/vi-workspace ~/bin/vi-workspace
-ln -s ~/config/scripts/kbgtl ~/bin/kbgtl
+ln -s ~/config/scripts/kbtgl ~/bin/kbtgl
 ln -s ~/config/scripts/todo ~/bin/todo
 ln -s ~/config/scripts/jrun ~/bin/jrun
 ln -s ~/config/scripts/jroot ~/bin/jroot
