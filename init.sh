21bad126aaf33d086833dc2721107044c9f7a567
bbrtj <brtastic.dev@gmail.com>
2022-02-11 20:45:16 +0100

    Add indic fonts and hs-pandoc

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 
 # Set up environment
 
-doas pkg install git xorg ncurses tmux pkgconf freetype2 fontconfig patch tree universal-ctags htop neofetch xbindkeys stalonetray the_silver_searcher py38-ranger firefox xf86-video-intel slock dmenu intel-backlight wpa_supplicant_gui freefont-ttf libsynaptics slim slim-freebsd-dark-theme pavucontrol xclip thunderbird gimp-app feh file-roller gmp gnome-screenshot bash xcompmgr neovim fusefs-ntfs drm-kmod twemoji-color-font-ttf ja-font-std zh-font-std webcamd v4l-utils v4l_compat calc
+doas pkg install git xorg ncurses tmux pkgconf freetype2 fontconfig patch tree universal-ctags htop neofetch xbindkeys stalonetray the_silver_searcher py38-ranger firefox xf86-video-intel slock dmenu intel-backlight wpa_supplicant_gui freefont-ttf libsynaptics slim slim-freebsd-dark-theme pavucontrol xclip thunderbird gimp-app feh file-roller gmp gnome-screenshot bash xcompmgr neovim fusefs-ntfs drm-kmod twemoji-color-font-ttf ja-font-std zh-font-std fonts-indic webcamd v4l-utils v4l_compat calc hs-pandoc
 
 mkdir ~/config/backup
 mv ~/.vimrc ~/config/backup/
@@ -33,3 +33,4 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 cp ~/config/local_config.example.sh ~/config/local_config.sh
 doas cp ~/config/scripts/brightness /usr/local/bin/brightness
 \curl -L https://install.perlbrew.pl | bash
+
