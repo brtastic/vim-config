623bdd6f8c61b948e944e7919f3a81ad3d188297
bbrtj <brtastic.dev@gmail.com>
2022-02-18 19:14:24 +0100

    Swap around vim plugins

--- vim.sh
+++ vim.sh
@@ -7,7 +7,7 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 # Clone plugins
 cd ~/.vim/bundle
 
-# Must have
+# Must have - vim won't function properly without those
 git clone https://github.com/junegunn/fzf.git
 git clone https://github.com/junegunn/fzf.vim.git
 git clone https://github.com/scrooloose/nerdtree.git
@@ -15,16 +15,16 @@ git clone https://github.com/moll/vim-bbye.git
 git clone https://github.com/morhetz/gruvbox.git
 git clone https://github.com/bbrtj/vim-fthelpers.git
 
-# Nice to have
+# Nice to have - very high value to my vim workflow
 git clone https://github.com/ludovicchabant/vim-gutentags.git
 git clone https://github.com/tpope/vim-commentary.git
 git clone https://github.com/tpope/vim-fugitive.git
 git clone https://github.com/tpope/vim-surround.git
 git clone https://github.com/editorconfig/editorconfig-vim
 git clone https://github.com/eugen0329/vim-esearch.git
-git clone https://github.com/majutsushi/tagbar.git
 
-# Optional
+# Optional - only occasionally useful
+git clone https://github.com/majutsushi/tagbar.git
 git clone https://github.com/bbrtj/vim-vorg-md.git
 git clone https://github.com/bbrtj/vim-jsonviewer.git
 git clone https://github.com/mattn/emmet-vim
